c6783b25ee16c86558f8b0fbde00498fedf9f591
JCaeta <63880187+JCaeta@users.noreply.github.com>
2022-05-11 19:54:45 -0300

    rama-caetano actualizada

--- src/main/java/grupo4/demo/PantallaIngresoMantenimientoCorrectivo.java
+++ src/main/java/grupo4/demo/PantallaIngresoMantenimientoCorrectivo.java
@@ -16,43 +16,8 @@
 //         return "index";
 //     }
 // }
+
 //------------------------------------------------------------------------ Version 2
-//package grupo4.demo;
-//import java.util.ArrayList;
-//import java.util.List;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import grupo4.demo.domain.Usuario;
-//
-//@Controller
-//public class ControladorInicio {
-//    @GetMapping("/")
-//    public String iniciar(Model model){
-//        var mensaje = "Mensaje de ejemplo";
-//
-//        model.addAttribute("mensaje", mensaje);
-//
-//        Usuario usr0 = new Usuario();
-//        usr0.setUsuario("usuario0");
-//        usr0.setClave("clave0");
-//
-//        Usuario usr1 = new Usuario();
-//        usr1.setUsuario("usuario1");
-//        usr1.setClave("clave1");
-//
-//        List<Usuario> usuarios = new ArrayList<>();
-//        usuarios.add(usr0);
-//        usuarios.add(usr1);
-//
-//        model.addAttribute("usuarios", usuarios);
-//
-//        return "index";
-//    }
-//}
-
-
-//------------------------------------------------------------------------ Version 3
 package grupo4.demo;
  import org.springframework.stereotype.Controller;
  import org.springframework.ui.Model;
