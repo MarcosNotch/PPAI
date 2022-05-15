bc67aca766333e339b0b72e023f4bcbe2fd76ee5
Marcos <marcoscanette1@gmail.com>
2022-05-15 13:40:15 -0300

    Caracteristica - Estado

---
+++ src/main/java/grupo4/demo/domain/Caracteristica.java
@@ -0,0 +1,13 @@
+package grupo4.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class Caracteristica {
+
+    private String nombre;
+    private String descripcion;
+
+    public void mostrarCaracteristica(){}
+
+}
